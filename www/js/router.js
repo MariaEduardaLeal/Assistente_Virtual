bfe071e1204b032f8cc81b907aba2c1111610d97
Maria Eduarda <eduardaleal753@gmail.com>
2023-12-08 19:48:30 -0300

    Front do PopUp de nova pergunta

--- www/js/router.js
+++ www/js/router.js
@@ -1,25 +1,44 @@
 
 var app = new Framework7({
-    // App root element
-    el: '#app',
-    // App Name
-    name: 'Betha Assistente',
-    // Enable swipe panel
-    panel: {
-      swipe: true,
+  // App root element
+  el: '#app',
+  // App Name
+  name: 'Betha Assistente',
+  // Enable swipe panel
+  panel: {
+    swipe: true,
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/index/',
+      url: 'index.html',
+      on: {
+        pageInit: function (e, page) {
+
+        }
+      }
+
     },
-    // Add default routes
-    routes: [
-      {
-        path: '/index/',
-        url: 'index.html',
-        
-      },
-      {
-        path: '/memorias/',
-        url: 'memorias.html',
-      },
-    ],
-  });
-  
-  var mainView = app.views.create('.view-main');
+    {
+      path: '/memorias/',
+      url: 'memorias.html',
+      on: {
+        pageInit: function (e, page) {
+          var searchbar = app.searchbar.create({
+            el: '.searchbar',
+            searchContainer: '.list',
+            searchIn: '.item-title',
+            on: {
+              search(sb, query, previousQuery) {
+                console.log(query, previousQuery);
+              }
+            }
+          });
+        }
+      }
+    },
+  ],
+});
+
+var mainView = app.views.create('.view-main');
